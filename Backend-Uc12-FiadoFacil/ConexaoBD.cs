a444d784379c47f457357def72d9c33fbf355b06
anthonydutra189 <anthonydutra189@gmail.com>
2026-08-12 22:11:12 -0300

    git ignore corrigido

---
+++ Backend-Uc12-FiadoFacil/ConexaoBD.cs
@@ -0,0 +1,13 @@
+﻿public class configuracaoBD
+{
+    private const string server = "srv793.hstgr.io";
+    private const string user = "u647084545_ti44_fiado";
+    private const string password = "Okp72eZ^0v";
+    private const string database = "u647084545_ti44_fiado";
+    private const string port = "3306";
+
+    public const string connectionString = $"Server={server};User ID={user};Password={password};Database={database};Port={port}";
+
+}
+
+
--- Backend-Uc12-FiadoFacil/Program.cs
+++ Backend-Uc12-FiadoFacil/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using MySqlConnector;
+
+using var connection = new MySqlConnection(configuracaoBD.connectionString);
+connection.Open();
+
+using var command = new MySqlCommand("SELECT * FROM testes;", connection);
+using var reader = command.ExecuteReader();
+while (reader.Read())
+    Console.WriteLine(reader.GetString(1));
